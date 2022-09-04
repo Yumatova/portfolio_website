24e5e64997258c5e1c5f7ae769ffe398aaaa03fb
Yumatova <109995247+Yumatova@users.noreply.github.com>
2022-09-03 18:43:47 -0700

    Js changes (#23)
    * Changes in JavaScript file
    
    -Deleted extra code at the beginning of js file.
    
    * CSS formatting
    
    * WIP
    
    * Icon resize
    
    -Resized LinkedIn and Behance icon;
    -Change the background color to white;
    - Aligned Relevant Skills and Tools column in the About section;
    - Centered header on index.html page.
    
    * Profile photo/icon size update
    
    -Set profile photo size;
    -Updated icons in the footer;
    -Changed About heading to My Story.
    
    * Delete extra code
    
    deleted part of code which I don't need

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,57 @@
+var mixedArray = [
+    1,
+    "Hello",
+    {},
+    ["nested array", "poop"]
+];
+
+var namesArray = [
+    "Vika",
+    "Anya",
+    "Karen",
+    "Jessica"
+]
+
+var vikaString = "Vika"
+
+var vikaInt = 27
+
+var vikaObj = {
+    "name": "Vika",
+    "age": 27,
+    "hobbies": "poop"
+}
+
+var vikasFriends = [
+    "Rodion",
+    "Oscar",
+    "Olyesa"
+]
+
+
+var toolsArray = [
+    "Adobe XD",
+    "Adobe Illustrator",
+    "Figma",
+    "Marvel",
+    "Miro",
+    "InVision"
+]
+
+console.log(`I know the following tools: ${toolsArray}`)
+
+console.log(`${vikaObj.name} wants to celebrate ${vikaObj.age} birthday and invite ${vikasFriends[0]}, ${vikasFriends[1]}, ${vikasFriends[2]}`)
+
+
+console.log('Vika wants to celebrate 27 birthday and invite Vika, Oscar, Olyesa')
+// Vika wants to celebrate 27 birthday and invite Rodion, Oscar, Olyesa
+
+
+
+
 console.log("Hello and welcome to my portfolio site!");
+console.log('He said: "Blablabla"') // He said: "Blablabla"
+
+console.log(namesArray[0]) // "Vika"
+
+console.log(mixedArray[3][1]) // "poop"
